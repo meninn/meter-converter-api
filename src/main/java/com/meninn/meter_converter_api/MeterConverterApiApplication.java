ef64687c1d82d7a3ef082bd612fc5af13598f993
meninn <gabrielm.boff@gmail.com>
2024-11-18 20:21:16 -0300

    update dockerfile

--- src/main/java/com/meninn/meter_converter_api/MeterConverterApiApplication.java
+++ src/main/java/com/meninn/meter_converter_api/MeterConverterApiApplication.java
@@ -2,6 +2,9 @@ package com.meninn.meter_converter_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MeterConverterApiApplication {
@@ -9,5 +12,18 @@ public class MeterConverterApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MeterConverterApiApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
 }
